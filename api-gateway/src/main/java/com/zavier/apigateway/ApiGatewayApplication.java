956c1363c8eba3a97dd392971381a8c0a17f40bf
zheng <765324639@qq.com>
2018-08-05 17:43:36 +0800

    网关添加请求过滤

--- api-gateway/src/main/java/com/zavier/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/zavier/apigateway/ApiGatewayApplication.java
@@ -1,8 +1,10 @@
 package com.zavier.apigateway;
 
+import com.zavier.apigateway.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -10,5 +12,10 @@ public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
     }
 }
